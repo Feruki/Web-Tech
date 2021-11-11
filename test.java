4c6ccdda0de9dac153efb8c9172fb2e8c7ed4d68
Yannick <84447020+Feruki@users.noreply.github.com>
2021-11-11 11:54:51 +0100

    Create test.java

---
+++ test.java
@@ -0,0 +1,44 @@
+
+
+
+
+public void main(String[] args) {
+  
+}
+
+
+
+
+
+
+
+public void claudius(String test) {
+  
+}
+
+
+
+
+
+
+
+public void justin(int num) {
+  
+}
+
+
+public void yannick(boolean maybe) {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
